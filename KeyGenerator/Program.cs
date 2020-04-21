a139d32a35b22a5d093ea9cc6404837218da5e6b
Alexander <bergersen.alexander@gmail.com>
2020-04-21 11:27:17 +0200

    Created a simple key generator

---
+++ KeyGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SymmetricKeyGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte[] secretKey = new byte[64];
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                while (true)
+                {
+                    provider.GetBytes(secretKey);
+                    var key = Convert.ToBase64String(secretKey);
+
+                    Console.WriteLine($"Key: {key}");
+                    Console.WriteLine("Enter r to generate a new key and any other key to quit....");
+                    var input = Console.ReadLine();
+                    if (input != "r")
+                        break;
+                }
+            }
+        }
+    }
+}
